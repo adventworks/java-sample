ad5e3b3089d0a82dfc0ff01001038a541b179304
Mateo Escobedo <mat.esc@outlook.com>
2017-06-29 00:41:05 -0400

    POM.xml fixes and test file

--- src/test/MyTest.java
+++ src/test/MyTest.java
@@ -1,14 +1,12 @@
 package test;
 import org.junit.*;
 
-public class Test {
+public class MyTest {
     @Test
     public void test_method_1() {
-        System.out.println("@Test - test_method_1");
     }
 
     @Test
     public void test_method_2() {
-        System.out.println("@Test - test_method_2");
     }
 }
