5765389e890c41a443938e0fafcb7680024aa496
Mateo Escobedo <mat.esc@outlook.com>
2017-06-29 00:18:57 -0400

    First commit

---
+++ src/test/Test.java
@@ -0,0 +1,14 @@
+package test;
+import org.junit.*;
+
+public class Test {
+    @Test
+    public void test_method_1() {
+        System.out.println("@Test - test_method_1");
+    }
+
+    @Test
+    public void test_method_2() {
+        System.out.println("@Test - test_method_2");
+    }
+}
